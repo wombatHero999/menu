eddd2b5a7c0aafcefd6bf92a31ff77887361b06e
DESKTOP-H488MRD\user1 <wombathero999@gmail.com>
2026-04-16 16:49:23 +0900

    cors -> exposedHeader추가

--- src/main/java/com/kh/menu/controller/MenuController.java
+++ src/main/java/com/kh/menu/controller/MenuController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -83,6 +84,7 @@ public class MenuController {
 					array = @ArraySchema(schema = @Schema(implementation = Menu.class))
 					)	
 			)
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<List<Menu>> menus(
 			@Parameter(description = "검색필터(type,taste)")
 			@RequestParam HashMap<String,Object> param
@@ -101,6 +103,7 @@ public class MenuController {
 	 *  - /menus/insert (x) => POST + /menus
 	 * */
 	@PostMapping("/menus")
+	@CrossOrigin(origins = "http://localhost:3000", exposedHeaders = "Location")
 	public ResponseEntity<Void> insertMenu(
 			@RequestBody MenuPost menu
 			){
@@ -130,6 +133,7 @@ public class MenuController {
 	 *  4. 조회결과가 존재하지 않을시 404에러상태 반환  
 	 *  */
 	@GetMapping("/menus/{id}")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<Menu> menuDetail(
 			@PathVariable long id){
 		Menu menu = menuService.menuDetail(id);
@@ -143,6 +147,8 @@ public class MenuController {
 	
 	//메뉴 수정
 	@PutMapping("/menus/{id}")
+	@CrossOrigin(origins = "http://localhost:3000" )
+	
 	public ResponseEntity<Void> updateMenu(
 			@RequestBody MenuPut menu,
 			@PathVariable long id
@@ -167,6 +173,7 @@ public class MenuController {
 	 *  4. 삭제 실패시 404에러상태 반환  
 	 *  */
 	@DeleteMapping("/menus/{id}")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<Void> deleteMenu(
 			@PathVariable long id
 			) throws NotFoundException{
